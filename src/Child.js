57b90b2a5a7aff8702c800df7390cfff98f8d374
redebron20 <redebron20@gmail.com>
2021-08-01 22:22:49 -0400

    Done.

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+// eslint-disable-next-line
 import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div
+      <div onClick={() => this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
